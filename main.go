ff92d4f278a8a6fbc0e6813c792e8b90cc0e16e5
RodneyPerez <rodney.the.programmer@gmail.com>
2020-11-21 13:28:24 -0700

    Added file flag to define path to quiz

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
+var fileName string
+
+func init() {
+	flag.StringVar(&fileName, "file", "problems.csv", "Path of quiz file")
+	flag.Parse()
+}
+
 func main() {
-	file, err := os.Open("problems.csv")
+	fmt.Println("Currently taking quiz located at this path ", fileName)
+	file, err := os.Open(fileName)
 
 	if err != nil {
 		log.Fatal(err)
